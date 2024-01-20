87fe5c5a33fd8c1a6377de75ab650b1d9040f373
jyoti-pandey8 <123292559+jyoti-pandey8@users.noreply.github.com>
2024-01-20 08:44:08 +0530

    Add initial Files

---
+++ scriptt.js
@@ -0,0 +1,13 @@
+// window.addEventListener("load", function(){
+//     setTimeout(
+//    function open(event){
+//     document.querySelector(".popupp").style.
+//     display = "block";
+//    },
+//    1000
+//     )
+// });
+// document.querySelector("#closee").addEventListener
+// ("click", function(){
+//     document.querySelector(".popupp").style.display = "none";
+// });
